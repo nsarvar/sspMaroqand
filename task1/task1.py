626607a74e7e660ebaaf3e5e51213f6f0b06a7ca
nsarvar <mr.sarvar_88@mail.ru>
2017-11-22 13:23:15 +0000

    commit

--- task1/task1.py
+++ task1/task1.py
@@ -5,13 +5,25 @@ import collections
 i=1;
 trans = collections.defaultdict(list)
 query = []
+prefix = {
+	'Uzmobile':['99','95'],
+	'Ucell':['93','94'],
+	'Beeline':['90', '91'],
+	'UMS': ['97'],
+	'Perfectum': ['98']
+}
 
+def getCompanyName(prfx):
+	for val in prefix:
+		if prfx in prefix[val]: return val
+
+## parsing the text from file
 with open("data.txt") as file:
 	for line in file:
 		if (i == 1) : n = int(line.strip())
 		if (i == 2) : k = int(line.strip())
 		if (i>2 and i <= n+2): 
-			transId, clientId, accountNo, amount, transDate, transTime = line.split('\t')
+	 		transId, clientId, accountNo, amount, transDate, transTime = line.split('\t')
 			trans[clientId, accountNo].append(accountNo)
 
 		if (i > n+2):
@@ -19,16 +31,18 @@ with open("data.txt") as file:
 
 		i +=1
 
-data = []
+data = {}
 j = 0
 for (clientId, accountNo), vals in sorted(trans.items()):
 	if(j != clientId):
-		data.append([clientId, accountNo, len(vals)]);	
+		data[clientId]=[clientId, accountNo, len(vals)];
 		j = clientId
 
-print(data)
-	#if(clientId in query):
-	#	print '%s\t%s\t%d' % (clientId, accountNo, len(vals))
-
+for val in query:
+	if val in data:
+		prfx = data[val][1][:2]
+		print "%s %s" % (data[val][1],getCompanyName(prfx))
+	else:
+		print "--"
 
 
